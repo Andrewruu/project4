6d4625992cc6a39a9065bb1cdb9fe986164b342a
Andrew Wang <Andrewruujin@gmail.com>
2023-07-19 18:47:54 -0400

    add user product to migrate

---
+++ db/migrate/20230719224709_create_user_product.rb
@@ -0,0 +1,12 @@
+class CreateUserProduct < ActiveRecord::Migration[7.0]
+  def change
+    create_table :user_products do |t|
+      t.integer :id
+      t.integer :user_id
+      t.integer :product_id
+      t.integer :quantity
+
+      t.timestamps
+    end
+  end
+end
