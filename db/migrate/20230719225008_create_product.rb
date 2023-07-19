5aaa09def9b8b37225cbfed7bf34519460f8f12e
Andrew Wang <Andrewruujin@gmail.com>
2023-07-19 19:04:18 -0400

    update migration product add name in create_product

--- db/migrate/20230719225008_create_product.rb
+++ db/migrate/20230719225008_create_product.rb
@@ -2,6 +2,7 @@ class CreateProduct < ActiveRecord::Migration[7.0]
   def change
     create_table :products do |t|
       t.decimal :price, precision: 10, scale: 2
+      t.string :name
       t.text :description
       t.string :image
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_19_225008) do
+  create_table "products", force: :cascade do |t|
+    t.decimal "price", precision: 10, scale: 2
+    t.string "name"
+    t.text "description"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_products", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "username"
+    t.string "password_digest"
+    t.decimal "fund", precision: 10, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
