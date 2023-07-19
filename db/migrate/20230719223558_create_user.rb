7025acf6380d73be7f6a3292f787794a6c637cc2
Andrew Wang <Andrewruujin@gmail.com>
2023-07-19 18:41:47 -0400

    add rails and react

---
+++ db/migrate/20230719223558_create_user.rb
@@ -0,0 +1,13 @@
+class CreateUser < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.integer :id
+      t.string :name
+      t.string :username
+      t.string :password_digest
+      t.decimal :fund, precision: 10, scale: 2
+
+      t.timestamps
+    end
+  end
+end
