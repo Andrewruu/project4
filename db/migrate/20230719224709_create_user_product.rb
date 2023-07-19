c015bff33e9c5927803191a2d2b3107dde6c320a
Andrew Wang <Andrewruujin@gmail.com>
2023-07-19 18:53:40 -0400

    update migration removed id

--- db/migrate/20230719224709_create_user_product.rb
+++ db/migrate/20230719224709_create_user_product.rb
@@ -1,7 +1,6 @@
 class CreateUserProduct < ActiveRecord::Migration[7.0]
   def change
     create_table :user_products do |t|
-      t.integer :id
       t.integer :user_id
       t.integer :product_id
       t.integer :quantity
--- db/migrate/20230719225008_create_product.rb
+++ db/migrate/20230719225008_create_product.rb
@@ -1,7 +1,6 @@
 class CreateProduct < ActiveRecord::Migration[7.0]
   def change
     create_table :products do |t|
-      t.integer :id
       t.decimal :price, precision: 10, scale: 2
       t.text :description
       t.string :image
